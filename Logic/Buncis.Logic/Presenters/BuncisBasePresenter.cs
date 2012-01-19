a4a428a6e80dd518ba6ae10b8b18b08f60d66898
hpinio <haritianpinio@gmail.com>
2012-01-19 16:36:11 +0800

    add presenter

---
+++ Logic/Buncis.Logic/Presenters/BuncisBasePresenter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Buncis.Framework.Mvp.Presenters;
+using WebFormsMvp;
+using Buncis.Core.Infrastructures;
+
+namespace Buncis.Logic.Presenters
+{
+    public abstract class BuncisBasePresenter<TView> : BasePresenter<TView> where TView : class, IView
+    {
+        protected ISystemSettings SystemSettings;
+
+        protected BuncisBasePresenter(ISystemSettings systemSettings, TView view)
+            : base(view)
+        {
+            SystemSettings = systemSettings;
+        }
+    }
+}
--- Logic/Buncis.Logic/Presenters/CategoryListingPresenter.cs
+++ Logic/Buncis.Logic/Presenters/CategoryListingPresenter.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Text;
 using Buncis.Logic.Views;
 using Buncis.Framework.Mvp.Presenters;
+using Buncis.Core.Infrastructures;
 
 namespace Buncis.Logic.Presenters
 {
-    public class CategoryListingPresenter : BasePresenter<ICategoryListingView>
+    public class CategoryListingPresenter : BuncisBasePresenter<ICategoryListingView>
     {
+        public CategoryListingPresenter(ISystemSettings systemSettings, ICategoryListingView view)
+            : base(systemSettings, view)
+        {
+
+        }
     }
 }
