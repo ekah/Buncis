a3628420e49bb41c755467c04678f43a28195b46
unknown <haritian.pinio@BEBALI-WS71.bullseye.com.au>
2012-07-20 19:44:47 +0800

    restructure #2

--- Web/Buncis.Web/bPanel/Pages/List.aspx.cs
+++ Web/Buncis.Web/bPanel/Pages/List.aspx.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Linq;
 using System.Web.UI.WebControls;
+using Buncis.Logic.Models;
+using Buncis.Logic.Presenters;
+using Buncis.Logic.Views;
 using Buncis.Logic.Views.Pages;
 using Buncis.Logic.Models.Pages;
 using Buncis.Web.Base;
@@ -11,12 +14,12 @@ using System.Web.UI.HtmlControls;
 
 namespace Buncis.Web.bPanel.Pages
 {
-    [PresenterBinding(typeof(BuncisPageListingPresenter), ViewType = typeof(IBuncisPageListingView))]
-	public partial class List : BaseLogicPage<BuncisPageListingModel>, IBuncisPageListingView
-    {
-        public void BindViewData()
-        {
-            
-        }
-    }
+	[PresenterBinding(typeof(FakePresenter), ViewType = typeof(IFakeView))]
+	public partial class List : BaseLogicPage<FakeModel>, IFakeView
+	{
+		public void BindViewData()
+		{
+
+		}
+	}
 }
