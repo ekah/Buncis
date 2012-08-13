512c67bc8f4bbcd433dea5b55a0efbb7c522e62d
haritian pinio <haritianpinio@gmail.com>
2012-08-13 22:14:02 +0800

    t4

---
+++ Core/Buncis.Framework.Core/Templates/IService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Buncis.Framework.Core.ViewModel;
+using Buncis.Framework.Core.SupportClasses;
+
+namespace Buncis.Framework.Core.Services.DailyBread
+{
+	public interface IDailyBreadService
+	{
+		IEnumerable<vDailyBread> GetDailyBreadsNotDeleted(int clientId);
+		vDailyBread GetDailyBread(int newsId);
+		ValidationDictionary<vDailyBread> DeleteDailyBread(int dailybreadId);
+		ValidationDictionary<vDailyBread> SaveDailyBread(int clientId, vDailyBread dailybread);
+	}
+}
---
+++ Web/Buncis.Web/bPanel/DailyBread/List.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Buncis.Logic.Models;
+using Buncis.Logic.Presenters;
+using Buncis.Web.Base;
+using Buncis.Logic.Views;
+using WebFormsMvp;
+
+namespace Buncis.Web.bPanel.DailyBread
+{
+    [PresenterBinding(typeof(FakePresenter), ViewType = typeof(IFakeView))]
+    public partial class List : BaseLogicPage<FakeModel>, IFakeView
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
